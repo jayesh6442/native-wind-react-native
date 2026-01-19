900b607bc8befce4353c5ec5699d1ef7304e1b7f
jayesh <jayeshsarvaiya9913@gmail.com>
2026-01-19 17:58:50 +0530

    END TO END WORKING NATIVE WIND CONFIG REACT NATIVE

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import "./global.css"
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        test
+      </Text>
+    </View>
+
+  );
+}
