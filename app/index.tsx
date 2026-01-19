3c4829acb7507d0945f11d9139f166feb947375a
jayesh <jayeshsarvaiya9913@gmail.com>
2026-01-19 20:30:51 +0530

    Minor changes

--- app/index.tsx
+++ app/index.tsx
@@ -3,12 +3,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
         test
       </Text>
     </View>
-
   );
 }
